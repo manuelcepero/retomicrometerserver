4b2a7bf805b50fa4143b14fb551d010a0aa4488e
Manuel Cepero Sánchez <manuel.cepero@hotmail.com>
2021-11-17 12:02:31 +0100

    Subida del proyecto

---
+++ src/main/java/com/actuator/retoactuatorserver/RetoactuatorserverApplication.java
@@ -0,0 +1,13 @@
+package com.actuator.retoactuatorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoactuatorserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoactuatorserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actuator/retoactuatorserver/RetoactuatorserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actuator.retoactuatorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoactuatorserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
